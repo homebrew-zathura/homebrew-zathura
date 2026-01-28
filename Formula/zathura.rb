9f9c6617ecb1c5d78757aa06046a06310851b617
JayanAXHF <sunil.chdry@gmail.com>
2026-01-28 17:08:27 +0530

    Revert "Reapply "chore(deps): bump up the version for zathura and its plugins (#165)""
    This reverts commit e4b77e9d29ecdfdb4c2f944580babb2b43bca068.

--- Formula/zathura.rb
+++ Formula/zathura.rb
@@ -1,8 +1,8 @@
 class Zathura < Formula
   desc "PDF viewer"
   homepage "https://pwmt.org/projects/zathura/"
-  url "https://github.com/pwmt/zathura/archive/refs/tags/0.5.14.tar.gz"
-  sha256 "e7a6bdfe90ed2d7405c4aa8e35f72ba00eda8306ea0e3dda6c02d0edbb00ce6f"
+  url "https://github.com/pwmt/zathura/archive/refs/tags/0.5.11.tar.gz"
+  sha256 "32540747a6fe3c4189ec9d5de46a455862c88e11e969adb5bc0ce8f9b25b52d4"
   license "Zlib"
   head "https://github.com/pwmt/zathura.git", branch: "develop"
 
